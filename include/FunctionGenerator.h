af39c0ce9f8a9f2ee7573dc3a797f94f8e24b508
Henri Casanova <henric@hawaii.edu>
2025-08-19 14:13:20 -1000

    Implemented a FunctionGenerator class

---
+++ include/FunctionGenerator.h
@@ -0,0 +1,43 @@
+#ifndef FUNCTIONGENERATOR_H
+#define FUNCTIONGENERATOR_H
+
+#include <boost/json.hpp>
+
+
+class FunctionGenerator {
+public:
+    FunctionGenerator() = default;
+    std::function<double(double, double)> get_function(boost::json::object& spec);
+
+    class AffineFunctor {
+    public:
+        AffineFunctor(double a, double b, double c): _a(a), _b(b), _c(c) {
+        }
+
+        double operator()(double x, double y) const {
+            return _a + _b * x + _c * y;
+        }
+
+    private:
+        double _a, _b, _c;
+    };
+
+    class QuadraticFunctor {
+    public:
+        QuadraticFunctor(double a, double b, double c, double d, double e): _a(a), _b(b), _c(c), _d(d), _e(e) {
+        }
+
+        double operator()(double x, double y) const {
+            return _a + _b * x + _c * y + _d * x * x + _e * y * y;
+        }
+
+    private:
+        double _a, _b, _c, _d, _e;
+    };
+
+private:
+    std::map<std::string, std::function<double(double, double)>> _function_registry;
+};
+
+
+#endif //FUNCTIONGENERATOR_H
---
+++ src/FunctionGenerator.cpp
@@ -0,0 +1,28 @@
+#include "FunctionGenerator.h"
+
+#include <iostream>
+
+std::function<double(double, double)> FunctionGenerator::get_function(boost::json::object& spec) {
+    std::string key = boost::json::serialize(spec);
+    // Lookup the registry in case we've done that one before
+    if (_function_registry.find(key) == _function_registry.end()) {
+        auto function_type = boost::json::value_to<std::string>(spec.at("type"));
+        if (function_type == "affine") {
+            _function_registry[key] = FunctionGenerator::AffineFunctor(
+                spec.at("parameters").as_object().at("a").as_double(),
+                spec.at("parameters").as_object().at("b").as_double(),
+                spec.at("parameters").as_object().at("c").as_double());
+        } else if (function_type == "quadratic") {
+            _function_registry[key] = FunctionGenerator::QuadraticFunctor(
+                spec.at("parameters").as_object().at("a").as_double(),
+                spec.at("parameters").as_object().at("b").as_double(),
+                spec.at("parameters").as_object().at("c").as_double(),
+                spec.at("parameters").as_object().at("d").as_double(),
+                spec.at("parameters").as_object().at("e").as_double());
+        } else {
+            throw std::invalid_argument("Unknown function type '" + function_type + "'");
+        }
+    }
+    // Return the function (which is really a functor)
+    return _function_registry[key];
+}
