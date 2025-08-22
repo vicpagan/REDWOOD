9b4a01ceab74c434c2682c7899acde18d7a1da04
Henri Casanova <henric@hawaii.edu>
2025-08-22 09:58:16 -1000

    Added a RunningJob and a RunningJobTracker method for easier (hoppefully) job tracking/bookkeeping

--- include/RunningJob.h
+++ include/RunningJob.h
@@ -14,21 +14,8 @@ namespace wrench {
         [[nodiscard]] std::string get_task_execution_option() const { return _task_execution_option; }
         [[nodiscard]] std::shared_ptr<CompoundJob> get_compound_job() const { return _compound_job; }
 
-        static void track_job(
-            const std::shared_ptr<CompoundJob>& compound_job,
-            const std::string& hostname,
-            const std::string& task_name,
-            const std::string& execution_option);
-
-        static void untrack_job(const std::shared_ptr<CompoundJob>& compound_job);
-        static void untrack_job(const std::string& hostname);
-
-        static std::shared_ptr<RunningJob> get_running_job(const std::shared_ptr<CompoundJob>& compound_job);
-        static std::shared_ptr<RunningJob> get_running_job(const std::string& hostname);
-
-        static void clear_all();
-
     private:
+        friend class RunningJobTracker;
         RunningJob(const std::shared_ptr<CompoundJob>& compound_job,
                    std::string task_name,
                    std::string task_execution_option) : _compound_job(compound_job),
@@ -41,15 +28,8 @@ namespace wrench {
         std::string _task_name;
         std::string _task_execution_option;
 
-        static std::map<std::string, std::shared_ptr<RunningJob>> _running_jobs;
-        static std::map<std::shared_ptr<CompoundJob>, std::shared_ptr<RunningJob>> _compound_job_to_running_job;
     };
 
-    inline void RunningJob::clear_all() {
-        _running_jobs.clear();
-        _compound_job_to_running_job.clear();
-    }
-    
 };
 
 
