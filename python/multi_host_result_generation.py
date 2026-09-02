2e82e0be19f9879c94622625191c4b9b78d8c88e
Henri Casanova <henric@hawaii.edu>
2026-09-01 16:48:09 -1000

    script enhancement

--- python/multi_host_result_generation.py
+++ python/multi_host_result_generation.py
@@ -387,6 +387,7 @@ def compare_two_things(frame: pd.DataFrame,
                        confidence: float,
                        resamples: int,
                        seed: int,
+                       break_ties = False,
                        muted: bool = False) -> bool:
     if not muted:
         print(f"\n** COMPARISON BETWEEN {kind}:{thing1} AND {kind}:{thing2} **")
@@ -397,7 +398,9 @@ def compare_two_things(frame: pd.DataFrame,
     # Identify all number of nodes
     thing1_never_loses = True
     thing1_wins_at_least_once = False
+    thing1_ties_but_always_in_top = True
     num_comparisons = 0
+
     num_nodes_values = sorted(list(set(frame["num_nodes"].to_numpy(dtype=int))))
     for num_nodes in num_nodes_values:
         if not muted:
@@ -423,6 +426,7 @@ def compare_two_things(frame: pd.DataFrame,
         average_loss_margin = 0
         largest_loss = 0
         largest_win = 0
+        average_estimate_for_ties = 0
         for algorithm_name in algorithm_names:
             heuristic, temporal_redundancy, reactive_rescheduling = algorithm_name.split("__")
             if kind == "all":
@@ -465,7 +469,7 @@ def compare_two_things(frame: pd.DataFrame,
 
             # print(f"Comparison to {algorithm_name}: {100.0*low:2f}%/{100.0 * estimate:.2f}%/{100.0*high:2f}%")
             if low > 0:
-                things1_wins_at_least_once = True
+                thing1_wins_at_least_once = True
                 num_wins += 1
                 average_win_margin += estimate
                 if estimate > largest_win:
@@ -477,6 +481,7 @@ def compare_two_things(frame: pd.DataFrame,
                 if estimate < largest_loss:
                     largest_loss = estimate
             else:
+                average_estimate_for_ties += estimate
                 num_ties += 1
 
         if num_losses > 0:
@@ -488,17 +493,28 @@ def compare_two_things(frame: pd.DataFrame,
         else:
             average_win_margin = 0
 
+        if num_ties > 0:
+            average_estimate_for_ties /= num_ties
+            thing1_ties_but_always_in_top = thing1_ties_but_always_in_top and (average_estimate_for_ties > 0.0)
+        else:
+            average_estimate_for_ties = 0
+
         if not muted:
             print(
                 f"      {num_wins} wins of {kind}:{thing1} over {kind}:{thing2} (average win margin {100.0 * average_win_margin:.2f}%, largest win: {100.0 * largest_win:.2f}%)")
             print(
                 f"      {num_losses} losses of {kind}:{thing1} to {kind}:{thing2} (average loss margin {100.0 * average_loss_margin:.2f}%, largest loss: {100.0 * largest_loss:.2f}%)")
             print(
-                f"      {num_ties} {kind}:{thing1} and {kind}:{thing2} ties")
+                f"      {num_ties} {kind}:{thing1} and {kind}:{thing2} ties (average estimate for ties: {100.0 * average_estimate_for_ties:.2f}%)")
 
-    return (num_comparisons > 0
-            and thing1_never_loses
-            and thing1_wins_at_least_once)
+    # print(f"num_comparisons: {num_comparisons}, thing1_never_loses={thing1_never_loses}, thing1_wins_at_least_once={thing1_wins_at_least_once}, thing1_ties_but_always_on_top={thing1_ties_but_always_in_top}")
+    return ((num_comparisons > 0
+             and thing1_never_loses
+             and thing1_wins_at_least_once) or
+            (break_ties and (
+                    num_comparisons > 0 and
+                    thing1_never_loses and
+                    thing1_ties_but_always_in_top > 0)))
 
 def report_algorithm_ranking(frame: pd.DataFrame, algorithm_names: list[str]):
 
@@ -975,7 +991,8 @@ def main() -> None:
                                                              args.confidence,
                                                              args.bootstrap_resamples,
                                                              args.seed,
-                                                             True)
+                                                             break_ties = True,
+                                                             muted = True)
                     if reference_dominates:
                         print(f"    ** REMOVING {comparison} FROM CONSIDERATION (DOMINATED BY {reference}) **")
                         algorithm_names.remove(comparison)
